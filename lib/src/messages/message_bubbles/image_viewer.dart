cb501ebee1a866a0b0de900b8b0b4d10823b6b21
suraj-bhavsar <suraj@questt.com>
2022-12-29 12:20:29 +0530

    ADD: Zoom feature in image viewer

--- lib/src/messages/message_bubbles/image_viewer.dart
+++ lib/src/messages/message_bubbles/image_viewer.dart
@@ -26,11 +26,25 @@ class ImageViewer extends StatelessWidget {
             IconThemeData(color: backIconColor ?? const Color(0xff3399FF)),
       ),
       backgroundColor: backgroundColor ?? const Color(0xffFFFFFF),
-      body: Center(
-        child: Image.network(messageObject.attachment!.fileUrl,
-            errorBuilder: (context, object, stackTrace) {
-          return const Center(child: Text("Failed To Load Image"));
-        }),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SizedBox(
+            height: constraints.maxHeight,
+            child: InteractiveViewer(
+              maxScale: 5,
+              minScale: 1,
+              child: Center(
+                child: Image.network(
+                  messageObject.attachment!.fileUrl,
+                  errorBuilder: (context, object, stackTrace) {
+                    return const Center(child: Text("Failed To Load Image"));
+                  },
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
