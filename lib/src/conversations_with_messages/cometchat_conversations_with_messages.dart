24fb4edda58c45b9d942147ef6bfb2d5d8f64824
Rajesh Korat <rajesh@questt.com>
2023-06-07 17:39:46 +0530

    UPDATE: hideAppBar & groupIDs filter logic

--- lib/src/conversations_with_messages/cometchat_conversations_with_messages.dart
+++ lib/src/conversations_with_messages/cometchat_conversations_with_messages.dart
@@ -47,7 +47,55 @@ class CometChatConversationsWithMessages extends StatefulWidget {
     this.theme,
     this.conversationsConfiguration,
     this.messageConfiguration,
+    this.hideAppBar = false,
+    this.shrinkWrap = false,
+    this.physics,
+    this.padding,
+    this.trailingIcon,
+    this.onTapTrailingIcon,
+    this.onTapUrl,
+    this.onTapItem,
+    this.onAddMediaClick,
+    this.onSendMessageClick,
+    this.onTapChatPageHeader,
+    this.groupIDs,
   }) : super(key: key);
+
+  ///[hideAppBar] toggle visibility of AppBar
+  final bool hideAppBar;
+
+  ///[padding] padding
+  final EdgeInsetsGeometry? padding;
+
+  ///[shrinkWrap] shrinkWrap for listview
+  final bool shrinkWrap;
+
+  ///[physics] physics for listview
+  final ScrollPhysics? physics;
+
+  ///[header trailing Icon]
+  final Widget? trailingIcon;
+
+  ///[header trailing Icon click]
+  final void Function(String groupId)? onTapTrailingIcon;
+
+  ///[onTapUrl] handle url tap inside message bubble
+  final Function(String)? onTapUrl;
+
+  ///[on Tap groups chat listing Item]
+  final Function(String guid)? onTapItem;
+
+  ///[add media option click in chat screen]
+  final Function(String guid, String mediaType)? onAddMediaClick;
+
+  ///[send button click in chat screen]
+  final Function(String guid, String messageType)? onSendMessageClick;
+
+  ///[on Tap chat page Header] handles the tap on chat page header
+  final void Function(String)? onTapChatPageHeader;
+
+  ///[selected groups ids] fetch only selected groups
+  final Set<String>? groupIDs;
 
   ///[user] if null will return [CometChatConversations] screen else will navigate to [CometChatMessages]
   final User? user;
@@ -83,9 +131,16 @@ class _CometChatConversationsWithMessagesState
             theme: widget.theme,
             messageConfiguration: widget.messageConfiguration);
     if (widget.user != null || widget.group != null) {
-      WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
+      WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
         _cometChatConversationsWithMessagesController.navigateToMessagesScreen(
-            user: widget.user, group: widget.group, context: context);
+          user: widget.user,
+          group: widget.group,
+          context: context,
+          onTapChatPageHeader: widget.onTapChatPageHeader,
+          onTapTrailingIcon: widget.onTapTrailingIcon,
+          trailingIcon: widget.trailingIcon,
+          onTapItem: widget.onTapItem,
+        );
       });
     }
   }
@@ -101,72 +156,99 @@ class _CometChatConversationsWithMessagesState
     return GetBuilder(
         init: _cometChatConversationsWithMessagesController,
         global: false,
-        dispose: (GetBuilderState<CometChatConversationsWithMessagesController> state) => state.controller?.onClose(),
+        dispose: (GetBuilderState<CometChatConversationsWithMessagesController>
+                state) =>
+            state.controller?.onClose(),
         builder: (CometChatConversationsWithMessagesController
             conversationsWithMessagesController) {
           conversationsWithMessagesController.context = context;
           return CometChatConversations(
-              conversationsProtocol:
-                  widget.conversationsConfiguration?.conversationsProtocol,
-              conversationsRequestBuilder: widget
-                  .conversationsConfiguration?.conversationsRequestBuilder,
-              activateSelection:
-                  widget.conversationsConfiguration?.activateSelection,
-              appBarOptions: widget.conversationsConfiguration?.appBarOptions,
-              controller: widget.conversationsConfiguration?.controller,
-              hideError: widget.conversationsConfiguration?.hideError,
-              stateCallBack: widget.conversationsConfiguration?.stateCallBack,
-              showBackButton:
-                  widget.conversationsConfiguration?.showBackButton ?? true,
-              theme: widget.conversationsConfiguration?.theme ?? widget.theme,
-              title: widget.conversationsConfiguration?.title,
-              subtitleView: widget.conversationsConfiguration?.subtitleView,
-              backButton: widget.conversationsConfiguration?.backButton,
-              avatarStyle: widget.conversationsConfiguration?.avatarStyle,
-              customSoundForMessages:
-                  widget.conversationsConfiguration?.customSoundForMessages,
-              disableSoundForMessages:
-                  widget.conversationsConfiguration?.disableSoundForMessages ??
-                      false,
-              disableReceipt:
-                  widget.conversationsConfiguration?.disableReceipt,
-              disableUsersPresence:
-                  widget.conversationsConfiguration?.disableUsersPresence,
-              datePattern: widget.conversationsConfiguration?.datePattern,
-              dateStyle: widget.conversationsConfiguration?.dateStyle,
-              deliveredIcon: widget.conversationsConfiguration?.deliveredIcon,
-              emptyText: widget.conversationsConfiguration?.emptyText,
-              emptyView: widget.conversationsConfiguration?.emptyView,
-              errorText: widget.conversationsConfiguration?.errorText,
-              errorView: widget.conversationsConfiguration?.errorView,
-              hideSeparator: widget.conversationsConfiguration?.hideSeparator,
-              listItemStyle: widget.conversationsConfiguration?.listItemStyle,
-              listItemView: widget.conversationsConfiguration?.listItemView,
-              loadingView: widget.conversationsConfiguration?.loadingView,
-              onSelection: widget.conversationsConfiguration?.onSelection,
-              options: widget.conversationsConfiguration?.options,
-              privateGroupIcon:
-                  widget.conversationsConfiguration?.privateGroupIcon,
-              protectedGroupIcon:
-                  widget.conversationsConfiguration?.protectedGroupIcon,
-              readIcon: widget.conversationsConfiguration?.readIcon,
-              selectionMode: widget.conversationsConfiguration?.selectionMode,
-              sentIcon: widget.conversationsConfiguration?.sentIcon,
-              statusIndicatorStyle:
-                  widget.conversationsConfiguration?.statusIndicatorStyle,
-              badgeStyle: widget.conversationsConfiguration?.badgeStyle,
-              receiptStyle: widget.conversationsConfiguration?.receiptStyle,
-              tailView: widget.conversationsConfiguration?.tailView,
-              typingIndicatorText:
-                  widget.conversationsConfiguration?.typingIndicatorText,
-              conversationsStyle:
-                  widget.conversationsConfiguration?.conversationsStyle ??
-                      const ConversationsStyle(),
-              onBack: widget.conversationsConfiguration?.onBack,
-              onError: widget.conversationsConfiguration?.onError,
-              onItemTap: widget.conversationsConfiguration?.onItemTap ?? conversationsWithMessagesController.onItemTap,
-              onItemLongPress: widget.conversationsConfiguration?.onItemLongPress,
-                      );
+            hideAppBar: widget.hideAppBar,
+            padding: widget.padding,
+            physics: widget.physics,
+            groupIDs: widget.groupIDs,
+            shrinkWrap: widget.shrinkWrap,
+            conversationsProtocol:
+                widget.conversationsConfiguration?.conversationsProtocol,
+            conversationsRequestBuilder:
+                widget.conversationsConfiguration?.conversationsRequestBuilder,
+            activateSelection:
+                widget.conversationsConfiguration?.activateSelection,
+            appBarOptions: widget.conversationsConfiguration?.appBarOptions,
+            controller: widget.conversationsConfiguration?.controller,
+            hideError: widget.conversationsConfiguration?.hideError,
+            stateCallBack: widget.conversationsConfiguration?.stateCallBack,
+            showBackButton:
+                widget.conversationsConfiguration?.showBackButton ?? true,
+            theme: widget.conversationsConfiguration?.theme ?? widget.theme,
+            title: widget.conversationsConfiguration?.title,
+            subtitleView: widget.conversationsConfiguration?.subtitleView,
+            backButton: widget.conversationsConfiguration?.backButton,
+            avatarStyle: widget.conversationsConfiguration?.avatarStyle,
+            customSoundForMessages:
+                widget.conversationsConfiguration?.customSoundForMessages,
+            disableSoundForMessages:
+                widget.conversationsConfiguration?.disableSoundForMessages ??
+                    true,
+            disableReceipt: widget.conversationsConfiguration?.disableReceipt,
+            disableUsersPresence:
+                widget.conversationsConfiguration?.disableUsersPresence,
+            datePattern: widget.conversationsConfiguration?.datePattern,
+            dateStyle: widget.conversationsConfiguration?.dateStyle,
+            deliveredIcon: widget.conversationsConfiguration?.deliveredIcon,
+            emptyText: widget.conversationsConfiguration?.emptyText,
+            emptyView: widget.conversationsConfiguration?.emptyView,
+            errorText: widget.conversationsConfiguration?.errorText,
+            errorView: widget.conversationsConfiguration?.errorView,
+            hideSeparator: widget.conversationsConfiguration?.hideSeparator,
+            listItemStyle: widget.conversationsConfiguration?.listItemStyle,
+            listItemView: widget.conversationsConfiguration?.listItemView,
+            loadingView: widget.conversationsConfiguration?.loadingView,
+            onSelection: widget.conversationsConfiguration?.onSelection,
+            options: widget.conversationsConfiguration?.options,
+            privateGroupIcon:
+                widget.conversationsConfiguration?.privateGroupIcon,
+            protectedGroupIcon:
+                widget.conversationsConfiguration?.protectedGroupIcon,
+            readIcon: widget.conversationsConfiguration?.readIcon,
+            selectionMode: widget.conversationsConfiguration?.selectionMode,
+            sentIcon: widget.conversationsConfiguration?.sentIcon,
+            statusIndicatorStyle:
+                widget.conversationsConfiguration?.statusIndicatorStyle,
+            badgeStyle: widget.conversationsConfiguration?.badgeStyle,
+            receiptStyle: widget.conversationsConfiguration?.receiptStyle,
+            tailView: widget.conversationsConfiguration?.tailView,
+            typingIndicatorText:
+                widget.conversationsConfiguration?.typingIndicatorText,
+            conversationsStyle:
+                widget.conversationsConfiguration?.conversationsStyle ??
+                    const ConversationsStyle(),
+            onBack: widget.conversationsConfiguration?.onBack,
+            onError: widget.conversationsConfiguration?.onError,
+            // onItemTap: widget.conversationsConfiguration?.onItemTap ??
+            //     conversationsWithMessagesController.onItemTap,
+            onItemTap: (conversation) {
+              User? user;
+              Group? group;
+              if (conversation.conversationType == ReceiverTypeConstants.user) {
+                user = (conversation.conversationWith as User);
+              } else if (conversation.conversationType ==
+                  ReceiverTypeConstants.group) {
+                group = (conversation.conversationWith as Group);
+              }
+              _cometChatConversationsWithMessagesController
+                  .navigateToMessagesScreen(
+                user: user,
+                group: group,
+                context: context,
+                onTapChatPageHeader: widget.onTapChatPageHeader,
+                onTapTrailingIcon: widget.onTapTrailingIcon,
+                trailingIcon: widget.trailingIcon,
+                onTapItem: widget.onTapItem,
+              );
+            },
+            onItemLongPress: widget.conversationsConfiguration?.onItemLongPress,
+          );
         });
   }
 }
